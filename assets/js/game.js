4242509b8d5e30a5ace3a06738685a56e73a8b03
eraymond1 <ericraymond@rocketmail.com>
2022-04-11 17:45:07 -0400

    added while loop to allow multip fights to continue until enemy robot dies

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,10 +16,14 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(enemyName) {
-    //Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+//Alert players that they are starting the round
+//window.alert("Welcome to Robot Gladiators!");
 
+
+var fight = function(enemyName) {
+
+  while(enemyHealth > 0) {
+    
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
 
   // if player choses to fight, then fight
@@ -64,9 +68,16 @@ var fight = function(enemyName) {
     } else {
         fight();
       }
+  }
+    
 };
 
 for (var i = 0; i <enemyNames.length; i++){
-  fight(enemyNames[i]);
+
+  var pickedEnemyName = enemyNames[i];
+
+  enemyHealth = 50;
+
+  fight(pickedEnemyName);
 }
 //fight();
