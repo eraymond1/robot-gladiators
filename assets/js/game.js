caa448bd09c1b8377fb0890ccf762d1bfc90e09a
eraymond1 <ericraymond@rocketmail.com>
2022-04-15 22:44:08 -0400

    fixed case sensitive issues with skip and fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,41 +6,52 @@
 
 
 
+var fightOrSkip = function() {
 
-//Alert players that they are starting the round
-//window.alert("Welcome to Robot Gladiators!");
+  // ask player if they'd like to fight or skip using fightOrSkip()
+  var promptFight = window.prompt('Woudl you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+  
+  promptFight = promptFight.toLocaleLowerCase();
+
+  // if player picks "skip" confirm and then stop the loop
+  if (promptFight === "skip") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+
+      // subtract money from playerMoney for skipping
+      playerInfo.money = playerInfo.money - 10;
+
+      // return true if player wants to leave
+      return true;
+
+      //shop();
+    }
+  }
+
+  return false;
+
+};
+
 
 
 var fight = function(enemy) {
 
-//debugger;
   while(playerInfo.health > 0 && enemy.health > 0) {
-    
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
 
-    if (promptFight === "skip" || promptFight === "SKIP") {
-
-      // confirm player wants to skip
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-    
-      // if yes (true), leave fight
-      if (confirmSkip) {
-
-        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-        
-        // subtract money from playerMoney for skipping
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-
-        console.log("playerMoney", playerInfo.money);
-
-        break;
-      }
-      // if no (false), ask question again by running fight() again
+    if (fightOrSkip()) {
+      //if true, leave fight by breaking loop
+      break;
     }
-
-    // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
-      // generate random damage value based on player's attack power
+    
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
       // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -81,9 +92,9 @@ var fight = function(enemy) {
         window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
       }
       // if player choses to skip
-    } else {
-        fight();
-      }
+    // } else {
+    //     fight();
+    //   }
   }
     
 };
